3be2b8e582b5c9493cad239620306abc73e07520
keiondramouton94-ship-it <keiondramouton94@gmail.com>
2026-07-05 14:57:23 -0500

    Update card.js

--- card.js
+++ card.js
@@ -1,64 +1,63 @@
 const dareCards = [
   
-Tell everyone your go to search on pornsites,
-Comment "I WANT YOU" under your crush's last birdy post and leave it up for the duration of the game,
-Tell us what turns you on,
-Read your last 3 text messages out loud,
-Name the strangest place you ever had sex,
-Name a song you know all the lyrics to. Now sing! If you fuck it up take a shot,
-Blow your breath in a player's face and let them describe what it smells like,
-Call a random in your phone and say "Daddy Am I your baby?" ,
-Drop your location to your ex and say "meet me here",
-Name 2 players of the game who you would have a threesome with,
-Tell us your 3 favorite sex positions,
+"Tell everyone your go to search on pornsites",
+"Comment "I WANT YOU" under your crush's last birdy post and leave it up for the duration of the game",
+"Tell us what turns you on",
+"Read your last 3 text messages out loud",
+"Name the strangest place you ever had sex",
+"Name a song you know all the lyrics to. Now sing! If you fuck it up take a shot",
+"Blow your breath in a player's face and let them describe what it smells like",
+"Call a random in your phone and say Daddy Am I your baby?",
+"Drop your location to your ex and say meet me here",
+"Name 2 players of the game who you would have a threesome with",
+"Tell us your 3 favorite sex positions",
 
   ];
 
 const truthCards = [
   
-Tell us what player in the room you have been intimate with or want to be,
-Have you ever messed with a married person,
-Have you ever had a threesome with your bestfriend,
-Have you ever hit a parked car and kept going,
-Would you rather a broke nigga who make you happy or a rich nigga you hate,
-What gang got the finest niggas,
-Do you like your toes sucked and do you like toes,
-Would you rather have a nice body with dry cat or a botched body with a waterfall,
-Tell the truth do you lick the gootch,
-Have you ever had sex with someone's man knowingly,
-Have you ever cried about a nigga cheating on you when you was cheating too,
+"Have you ever messed with a married person",
+"Have you ever had a threesome with your bestfriend",
+"Have you ever hit a parked car and kept going",
+"Would you rather a broke nigga who make you happy or a rich nigga you hate",
+"What gang got the finest niggas",
+"Do you like your toes sucked and do you like toes",
+"Would you rather have a nice body with dry cat or a botched body with a waterfall",
+"Tell the truth do you lick the gootch",
+"Have you ever had sex with someone's man knowingly",
+"Have you ever cried about a nigga cheating on you when you was cheating too",
 
   ];
 
 const shotCards = [
 
-Take a shot if you ever got caught having sex,
-Take a shot if you have that scammer role,
-Take a shot if you ever had to pop a bitch behind your nigga,
-Take a shot if you ever took a plan b,
-Take a shot if you fucked a nigga on EMS,
-Name the last 2 guys you had sex with or take 4 shots,
-Anyone that is younger than you must take a shot,
-Name 5 States in 5 seconds or take a shot,
-Take a shot if you still sober with your boring ass,
-Take a shot if you ever been arrested,
-Take a shot if you spit it out when your partner cums with your childish ass,
+"Take a shot if you ever got caught having sex",
+"Take a shot if you have that scammer role",
+"Take a shot if you ever had to pop a bitch behind your nigga",
+"Take a shot if you ever took a plan b",
+"Take a shot if you fucked a nigga on EMS",
+"Name the last 2 guys you had sex with or take 4 shots",
+"Anyone that is younger than you must take a shot",
+"Name 5 States in 5 seconds or take a shot",
+"Take a shot if you still sober with your boring ass",
+"Take a shot if you ever been arrested",
+"Take a shot if you spit it out when your partner cums with your childish ass",
 
   ];
 
 const wildCards = [
 
-Give a lap dance to a player of your choice,
-Get up and race the person to your right loser takes 4 shots,
-Twerk off!2 players must compete. All other players must vote,
-Call any businesses and put them on the phone with eachother,
-Do a naked lap around the house,
-Lick the closest pussy next to you or take 2 shots,
-Pick a person to say a prayer for us to get through this wild game,
-You and the person to your right must have a 30 second dance battle to soulja boy crank that,
-Let the group vote on a harmless challenge,
-Let the group decide your next birdy post..post it,
-Go live on social media for 30 seconds,
+"Give a lap dance to a player of your choice,
+"Get up and race the person to your right loser takes 4 shots",
+"Twerk off!2 players must compete. All other players must vote",
+"Call any businesses and put them on the phone with eachother",
+"Do a naked lap around the house",
+"Lick the closest pussy next to you or take 2 shots",
+"Pick a person to say a prayer for us to get through this wild game",
+"You and the person to your right must have a 30 second dance battle to soulja boy crank that",
+"Let the group vote on a harmless challenge",
+"Let the group decide your next birdy post..post it",
+"Go live on social media for 30 seconds",
 
   ];
 
